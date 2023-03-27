0d590beb44f4a0babce7f37733a642f792e0ed2b
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-26 20:07:40 -0400

    feat: add readable attributes name and category

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Dish do
 
     expect(dish).to be_a Dish
   end
+
+  it "has_readable_attributes" do
+  dish = Dish.new("Couscous Salad", :appetizer)
   
+  expect(dish.name).to eq("Couscous Salad")
+  expect(dish.category).to eq(:appetizer)
+  end
   
 end
