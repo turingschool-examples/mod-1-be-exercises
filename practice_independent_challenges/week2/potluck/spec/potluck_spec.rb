7fc3ad9322b58f791a85f18db7a12f4eb2762b3c
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-26 20:27:05 -0400

    Test: write test for add_dish method

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe Potluck do
     expect(potluck.dishes).to eq([])
   end
 
+  it "adds_new_dishes" do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    
+    expect(potluck.dishes.length).to eq(2)
+  end
 end
