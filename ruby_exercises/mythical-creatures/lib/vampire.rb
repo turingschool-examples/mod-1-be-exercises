64591c537851ad2139b8ff54de3eb784dbfdef0f
Davis Weimer <davisweimer@gmail.com>
2023-04-26 15:48:07 -0600

    Create Vampire Class

---
+++ ruby_exercises/mythical-creatures/lib/vampire.rb
@@ -0,0 +1,18 @@
+class Vampire
+    attr_reader :name, :pet
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+        @drink_counter = 0
+    end
+    def drink
+        @drink_counter += 1
+    end
+    def thirsty
+        @thirsty
+        return false if @drink_counter >= 1
+        true
+    end
+
+end    
