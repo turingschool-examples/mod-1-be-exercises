7cf1636fad9ac36e5c202e4d2ba975e8d3e5cd3a
Weston Schutt <wtschutt@gmail.com>
2023-05-01 11:50:00 -0600

    Add completed unicorn mc exercise

---
+++ ruby_exercises/mythical-creatures/lib/unicorn.rb
@@ -0,0 +1,16 @@
+class Unicorn
+    attr_reader :name, :color
+
+    def initialize(name, color="silver")
+        @name = name
+        @color = color
+    end
+
+    def silver?
+        @color == 'silver'
+    end
+
+    def say(words)
+        "**;* #{words} **;*"
+    end
+end
