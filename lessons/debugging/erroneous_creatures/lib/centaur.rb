1d7df19286911e74d43b9938ebc907d96db01625
Weston Schutt <wtschutt@gmail.com>
2023-05-12 09:46:24 -0600

    Add completed erroneous creatures exercieses

--- lessons/debugging/erroneous_creatures/lib/centaur.rb
+++ lessons/debugging/erroneous_creatures/lib/centaur.rb
@@ -9,6 +9,7 @@ class Centaur
     @standing = standing
     @laying = laying
     @sleeping = sleeping
+    @crankiness = 0
   end
 
   def shoot
@@ -31,9 +32,9 @@ class Centaur
 
   def cranky?
     if @crankiness < 3
-      "false"
+      false
     else
-      "true"
+      true
     end
   end
 
@@ -45,8 +46,8 @@ class Centaur
     if @standing
       "NO!"
     else
-      @sleeping = true
       @crankiness = 0
+      @sleeping = true
     end
   end
 
