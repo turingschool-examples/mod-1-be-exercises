72329eda91128af8937396d64e3d0cd242ac6276
Davis Weimer <davisweimer@gmail.com>
2023-05-17 12:10:32 -0600

    feature: Create RSpec Itr-1 for Dish class

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -1,5 +1,22 @@
 require './lib/dish'
 
 RSpec.describe Dish do
+  it 'exists' do
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    expect(dish).to be_instance_of(Dish)
+  end
+
+  it 'is a couscous salad' do
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    expect(dish.name).to eq("Couscous Salad")
+  end
   
+  it 'is an appetizer' do
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    expect(dish.category).to eq(:appetizer)
+  end
+
 end
