1d813176cf82fe0e8908f8f64eb672fda055e2c7
Melony Franchini <meltravelz@gmail.com>
2023-06-12 18:13:55 -0600

    Add Iteration 2 - reunion

--- practice_independent_challenges/week5/reunion/lib/activity.rb
+++ practice_independent_challenges/week5/reunion/lib/activity.rb
@@ -13,4 +13,26 @@ class Activity
   def total_cost
     @participants.values.sum
   end
+
+  def split 
+    total_cost / @participants.count #.size #.length
+  end
+
+  # OPTION 1
+  def owed
+    @participants.transform_values do |money_spent|
+      (money_spent - split) * -1 #OR split - money_spent
+    end
+  end
+
+  # OPTION 2
+  # def owed
+  #   part_owed = Hash.new
+
+  #   @participants.each do |participant, money|
+  #     part_owed[participant] = split - money
+  #   end
+
+  #   part_owed
+  # end
 end
--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -17,6 +17,9 @@ RSpec.describe Activity do
   describe "#add_participant" do
     it "can add a participant" do
       activity = Activity.new("Brunch")
+
+      expect(activity.participants).to eq({})
+
       activity.add_participant("Maria", 20)
 
       expect(activity.participants).to eq({"Maria" => 20})
@@ -37,6 +40,30 @@ RSpec.describe Activity do
       activity.add_participant("Luther", 40)
 
       expect(activity.total_cost).to eq(60)
+    end
+  end
+
+  describe "#split" do
+    it "can evenly split the total cost between all participants" do
+      activity = Activity.new("Brunch")
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      expect(activity.total_cost).to eq(60) 
+      expect(activity.split).to eq(30)
+    end
+  end
+
+  describe "#owed" do
+    it "returns the amount a participant is owed or owes" do
+      activity = Activity.new("Brunch")
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      expected = { "Maria" => 10, "Luther" => -10 }
+
+      expect(activity.owed).to eq(expected)
+      # expect(activity.owed).to eq({ "Maria" => 10, "Luther" => -10 })
     end
   end
 end
