b5bd6d0dd0e2bceb6b868eda893463e1cce47926
Antoine Aube <aaube3@gmail.com>
2023-06-21 19:47:15 -0600

    add solutions to none enumerables

--- ruby_exercises/enumerables/exercises_1/spec/none_pattern_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/none_pattern_spec.rb
@@ -9,37 +9,52 @@ RSpec.describe 'none pattern' do
     expect(none_broken).to eq(true)
   end
 
-  xit 'double negative' do
+  it 'double negative' do
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = true
     numbers.each do |number|
       # Your code goes here
+      not_none_negative = false if number.negative? == true 
     end
     expect(not_none_negative).to eq(false)
   end
 
-  xit 'none are negative' do
+  it 'none are negative' do
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
     # Your code goes here
+    numbers.each do |number|
+      none_negative = false if number.negative? == true 
+    end
     expect(none_negative).to eq(true)
   end
 
-  xit 'none shall pass' do
+  it 'none shall pass' do
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
     # Your code goes here
+    none_shall_pass = false 
+    critters.each do |critter|
+      none_shall_pass = true if critter == critter 
+    end
     expect(none_shall_pass).to eq(true)
   end
 
-  xit 'one or more shall pass' do
+  it 'one or more shall pass' do
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = true 
+    phrases.each do |phrase|
+      none_shall_pass = false if phrase == "must stay" or "tarried"
+    end
+
     expect(none_shall_pass).to eq(false)
   end
 
-  xit 'none even' do
+  it 'none even' do
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = false
+    numbers.each do |number|
+      none_even = true if number.even? == false 
+    end
     expect(none_even).to eq(true)
   end
 end
--- ruby_exercises/enumerables/exercises_1/spec/none_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/none_spec.rb
@@ -8,35 +8,46 @@ RSpec.describe 'none' do
     expect(none_broken).to eq(true)
   end
 
-  xit 'double negative' do
+  it 'double negative' do
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = numbers.none? do |number|
-      # Your code goes here
+      number.negative? == true 
     end
     expect(not_none_negative).to eq(false)
   end
 
-  xit 'none are negative' do
+  it 'none are negative' do
     numbers = [9, 3, 1, 8, 3, 3, 5]
-    # Your code goes here
+    none_negative = numbers.none? do |number|
+      number.negative? == true 
+    end
+
     expect(none_negative).to eq(true)
   end
 
-  xit 'none shall pass' do
+  it 'none shall pass' do
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
     # Your code goes here
+    none_shall_pass = critters.none? do |critter|
+      critter == "pass"
+    end
     expect(none_shall_pass).to eq(true)
   end
 
-  xit 'one or more shall pass' do
+  it 'one or more shall pass' do
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
     # Your code goes here
+    none_shall_pass = phrases.none? do |phrase|
+      phrase == "must stay" or "tarried"
+    end
     expect(none_shall_pass).to eq(false)
   end
 
-  xit 'none even' do
+  it 'none even' do
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = numbers.none? do |number|
+      number.even?
+    end
     expect(none_even).to eq(true)
   end
 end
