bab61cb8c491548bafb41b35ead58ee9e7d26b60
Antoine Aube <aaube3@gmail.com>
2023-06-26 19:19:03 -0700

    add solutions to one enumerable

--- ruby_exercises/enumerables/exercises_1/spec/one_pattern_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/one_pattern_spec.rb
@@ -10,41 +10,58 @@ RSpec.describe 'one pattern' do
     expect(exactly_one).to eq(true)
   end
 
-  xit 'not even one ring' do
+  it 'not even one ring' do
     ornaments = ["bracelet", "anklet", "earring"]
     count = 0
     ornaments.each do |ornament|
+      count += 1 if ornament == "ring"
       # Your code goes here
     end
     exactly_one_ring = count == 1
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'not just one ring' do
+  it 'not just one ring' do
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
     count = 0
     # Your code goes here
+    ornaments.each do |ornament|
+      count += 1 if ornament == "ring"
+      # Your code goes here
+    end
     exactly_one_ring = count == 1
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'one time' do
+  it 'one time' do
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
     # Your code goes here
+    count = 0
+    words.each do |word|
+      count += 1 if word == "time"
+    end
     exactly_one_time = count == 1
     expect(exactly_one_time).to eq(true)
   end
 
-  xit 'one double digit number' do
+  it 'one double digit number' do
     numbers = [8, 2, 10, 333, 9, 101]
     # Your code goes here
+    count = 0
+    numbers.each do |num|
+      count += 1 if num.digits.length == 2
+    end
     exactly_one_double_digit = count == 1
     expect(exactly_one_double_digit).to eq(true)
   end
 
-  xit 'not one even number' do
+  it 'not one even number' do
     numbers = [3, 20, 81, 10, 391, 32]
     # Your code goes here
+    count = 0
+    numbers.each do |num|
+      count += 1 if num % 2 == 0 
+    end
     exactly_one_even_number = count == 1
     expect(exactly_one_even_number).to eq(false)
   end
--- ruby_exercises/enumerables/exercises_1/spec/one_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/one_spec.rb
@@ -8,35 +8,40 @@ RSpec.describe 'one' do
     expect(exactly_one).to eq(true)
   end
 
-  xit 'not even one ring' do
+  it 'not even one ring' do
     ornaments = ["bracelet", "anklet", "earring"]
     exactly_one_ring = ornaments.one? do |ornament|
       # Your code goes here
+      ornament == "ring"
     end
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'not just one ring' do
+  it 'not just one ring' do
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
     # Your code goes here
+    exactly_one_ring = ornaments.one? {|ornament| ornament == "ring"}
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'one time' do
+  it 'one time' do
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
     # Your code goes here
+    exactly_one_time = words.one? {|word| word == "time"}
     expect(exactly_one_time).to eq(true)
   end
 
-  xit 'one double digit number' do
+  it 'one double digit number' do
     numbers = [8, 2, 10, 333, 9, 101]
     # Your code goes here
+    exactly_one_double_digit = numbers.one? {|number| number.digits.length == 2}
     expect(exactly_one_double_digit).to eq(true)
   end
 
-  xit 'not even one number' do
+  it 'not even one number' do
     numbers = [3, 20, 81, 10, 391, 32]
     # Your code goes here
+    exactly_one_even_number = numbers.one? {|number| number % 2 == 0}
     expect(exactly_one_even_number).to eq(false)
   end
 end
