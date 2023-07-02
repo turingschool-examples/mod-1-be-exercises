c1f4dd38bc37d420f751ebae9c36653fb540d963
Antoine Aube <aaube3@gmail.com>
2023-07-02 17:31:41 -0600

    add solution to select enumerable

--- ruby_exercises/enumerables/exercises_1/spec/reject_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/reject_spec.rb
@@ -8,83 +8,97 @@ RSpec.describe 'reject' do
     expect(filtered).to eq([2, 93, 7, 1, 31, 368])
   end
 
-  xit 'removes vowels' do
+  it 'removes vowels' do
     letters = ["a", "l", "l", " ", "y", "o", "u", "r", " ", "b", "a", "s", "e", " ", "a", "r", "e", " ", "b", "e", "l", "o", "n", "g", " ", "t", "o", " ", "u", "s"]
     remaining = letters.reject do |letter|
       # Your code goes here
+        letter.match(/[aeiouy]/)
     end
     expect(remaining).to eq(["l", "l", " ", "r", " ", "b", "s", " ", "r", " ", "b", "l", "n", "g", " ", "t", " ", "s"])
   end
 
-  xit 'remove numbers divisible by 3' do
+  it 'remove numbers divisible by 3' do
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     # Your code goes here
+    remaining = numbers.reject {|number| number % 3 == 0}
     expect(remaining).to eq([1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20])
   end
 
-  xit 'remove words longer tghan three letters' do
+  it 'remove words longer tghan three letters' do
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     # Your code goes here
+    selected = words.reject {|word| word.length > 3}
     expect(selected).to eq(["bad", "cat", "dog", "red"])
   end
 
-  xit 'remove words ending in e' do
+  it 'remove words ending in e' do
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
     # Your code goes here
+    selected = words.reject {|word| word[-1] == "e"}
     expect(selected).to eq(["you", "thinking", "belt", "sing"])
   end
 
-  xit 'remove words ending in ing' do
+  it 'remove words ending in ing' do
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
     # Your code goes here
+    selected = words.reject {|word| word.end_with?("ing")}
     expect(selected).to eq(["finger", "drought", "bingo", "purposeful"])
   end
 
-  xit 'remove words containing e' do
+  it 'remove words containing e' do
     words = ["four", "red", "five", "blue", "pizza", "purple"]
     # Your code goes here
+    selected = words.reject {|word| word.include?("e")}
     expect(selected).to eq(["four", "pizza"])
   end
 
-  xit 'remove dinosaurs' do
+  it 'remove dinosaurs' do
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
     # Your code goes here
+    notasaurus = animals.reject {|animal| animal.end_with?("saurus")}
     expect(notasaurus).to eq(["narwhal", "eel"])
   end
 
-  xit 'remove numbers' do
+  it 'remove numbers' do
     elements = ["cat", "dog", 23, 81.1, 56, "aimless", 43]
     # Your code goes here
+    not_numbers = elements.reject {|element| element.is_a?(Integer) || element.is_a?(Float)}
     expect(not_numbers).to eq(["cat", "dog", "aimless"])
   end
 
-  xit 'remove floats' do
+  it 'remove floats' do
     elements = ["cat", "dog", 32.333, 23, 56, "aimless", 43.2]
     # Your code goes here
+    not_numbers = elements.reject {|element| element.is_a?(Float)}
     expect(not_numbers).to eq(["cat", "dog", 23, 56, "aimless"])
   end
 
-  xit 'remove animals starting with a vowel' do
+  it 'remove animals starting with a vowel' do
     animals = ["aardvark", "bonobo", "cat", "dog", "elephant"]
     # Your code goes here
+    remaining = animals.reject { |element| element.start_with?(/[aeiou]/) }
     expect(remaining).to eq(["bonobo", "cat", "dog"])
   end
 
-  xit 'remove upcased words' do
+  it 'remove upcased words' do
     words = ["CAT", "dog", "AIMLESS", "Trevor", "butter"]
     # Your code goes here
+    remaining = words.reject{|word| word == word.upcase}
     expect(remaining).to eq(["dog", "Trevor", "butter"])
   end
 
-  xit 'remove arrays' do
+  it 'remove arrays' do
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
     # Your code goes here
+    remaining = elements.reject {|element| element.is_a?(Array)}
     expect(remaining).to eq(["CAT", 23, "AIMLESS", 43, "butter"])
   end
-
-  xit 'remove hashes' do
+  
+  it 'remove hashes' do
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
     # Your code goes here
+    
+    remaining = elements.reject {|element| element.is_a?(Hash)}
     expect(remaining).to eq(["cat", 23, "aimless", 43])
   end
 end
