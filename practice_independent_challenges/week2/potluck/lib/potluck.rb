d448151140362e73f6f1d04bcc297839a768b23d
Antoine Aube <aaube3@gmail.com>
2023-07-12 14:29:12 -0600

    test: potluck has a date instance variable

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -1,6 +1,7 @@
 class Potluck
-  attr_reader :date
+  attr_reader :date, :dishes
   def initialize(date)
     @date = date
+    @dishes = []
   end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe Potluck do
   describe "iteration_2" do 
     it "is an instance of Potluck" do
       potluck = Potluck.new("7-13-18")
-
+      
       expect(potluck).to be_a(Potluck)
+    end
+    
+    it "has a date instance variable" do 
+      potluck = Potluck.new("7-13-18")
+
+      expect(potluck.date).to eq("7-13-18")
     end
   end
 end
