c53a41c7bbc1750a438942c56e18e785bf27faf9
Antoine Aube <aaube3@gmail.com>
2023-07-12 13:51:41 -0600

    Intial commit

---
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -0,0 +1,23 @@
+require './lib/dish'
+require 'pry'
+
+RSpec.describe Dish do 
+  describe "iteration_1" do 
+    it "is an instance of Dish" do 
+      dish = Dish.new("Couscous Salad", :appetizer)
+      expect(dish).to be_a(Dish)
+    end
+    
+    it "dish has a name" do 
+      dish = Dish.new("Couscous Salad", :appetizer)
+      
+      expect(dish.name).to eq("Couscous Salad")
+    end
+    
+    it "has a dish category" do 
+      dish = Dish.new("Couscous Salad", :appetizer)
+
+      expect(dish.category).to eq(:appetizer)
+    end
+  end
+end
