1701599c8edd069ef8634395ab0deb25df800fba
Antoine Aube <aaube3@gmail.com>
2023-07-12 14:57:58 -0600

    test: potluck has a menu that returns a hash with dish categories

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -54,4 +54,24 @@ RSpec.describe Potluck do
       expect(potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
     end 
   end
+  describe "iteration_4" do 
+    it "has a menu that returns a has by category of dish" do 
+      potluck = Potluck.new("7-13-18")
+      couscous_salad = Dish.new("Couscous Salad", :appetizer)
+      summer_pizza = Dish.new("Summer Pizza", :appetizer)
+      roast_pork = Dish.new("Roast Pork", :entre)
+      cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+      candy_salad = Dish.new("Candy Salad", :dessert)
+      bean_dip = Dish.new("Bean Dip", :appetizer)
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(candy_salad)
+      potluck.add_dish(bean_dip)          
+
+      expect(potluck.menu).to be_a Hash
+      expect(potluck.ratio(:appetizer)).to eq(50.0)
+    end
+  end
 end
