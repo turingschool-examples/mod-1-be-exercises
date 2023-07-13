4f8322d05de08b0831231e3d84e1cac5eb100dd1
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-12 19:39:04 -0600

    test: add additional patrons to verify the method works to return true if more patrons than capacity are added

--- practice_independent_challenges/week1/venue/spec/venue_spec.rb
+++ practice_independent_challenges/week1/venue/spec/venue_spec.rb
@@ -49,11 +49,16 @@ describe Venue do
   end
 
   describe '#over_capacity' do
-    it 'can return whether the venue is over capacity'
-    venue = Venue.new('Bluebird', 4)
-    venue.add_patron('Mike')
-    venue.add_patron('Megan')
-    venue.add_patron('Bob')
-    expect(venue.over_capacity?).to eq(false)
+    it 'can return whether the venue is over capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      expect(venue.over_capacity?).to eq(false)
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      expect(venue.over_capacity?).to eq(true)
+    end
+
   end
 end
