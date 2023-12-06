98b6adec1fdc7a8509d475b392e5f5b7b1352a56
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-06 15:43:10 -0500

    Refactor: remove skip from tests

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,10 @@
 class Venue
+   attr_reader :name, :capacity, :patrons
+
+   def initialize(name, capacity)
+      @name = name
+      @capacity = capacity
+      @patrons = []
+   end
+
 end
--- practice_independent_challenges/week1/venue/spec/venue_spec.rb
+++ practice_independent_challenges/week1/venue/spec/venue_spec.rb
@@ -4,25 +4,21 @@ require './lib/venue'
 describe Venue do
   describe '#initialize' do
     it 'is a venue' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,7 +28,6 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -44,7 +39,6 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
