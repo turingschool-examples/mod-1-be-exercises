e31dc6b8703904aea95ed00900287a36bd4ea75a
Juliet Eyraud <juliet@turing.edu>
2024-05-30 16:48:41 -0500

    reset module file

--- lessons/modules/orders/lib/deliverable.rb
+++ lessons/modules/orders/lib/deliverable.rb
@@ -1,20 +1,2 @@
 # ./deliverable.rb
-module Deliverable
-  def confirmation(thing)
-    "You got #{thing}."
- end
 
- def review
-    "Please rate your order within 30 days." 
- end
-
- def delivery
-  if self.class == AmazonOrder
-    "Your order will arrive in 2 business days."
-  elsif self.class == GrubhubOrder
-    "Your food will arrive in 45-60 minutes."
-  else
-    "We don't know when your order will be delivered"
-  end
- end
-end
